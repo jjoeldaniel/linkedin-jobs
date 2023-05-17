4c677a5c25372317a14e4dee8c0ac2cfed303761
Joel Daniel Rico <joeldanielrico@csu.fullerton.edu>
2023-05-17 03:46:32 -0700

    added date_added

--- src/job.py
+++ src/job.py
@@ -1,3 +1,6 @@
+import datetime
+
+
 class Job:
     """Represents a job posting on LinkedIn"""
 
@@ -8,14 +11,16 @@ class Job:
         location: str = "Mountain View, CA",
         link: str = "https://www.google.com/",
         date: str = "Today",
+        date_added: str = str(datetime.datetime.now()),
     ) -> None:
         self.title = title
         self.company = company
         self.location = location
         self.link = link
         self.date = date
+        self.date_added = date_added
 
     def __str__(self):
         """Returns a string representation of the Job object"""
 
-        return f"Title: {self.title}\nCompany: {self.company}\nLocation: {self.location}\nLink: {self.link}\nDate Posted: {self.date}\n"
+        return f"Title: {self.title}\nCompany: {self.company}\nLocation: {self.location}\nLink: {self.link}\nDate Posted: {self.date}\nDate Added: {self.date_added}\n"
