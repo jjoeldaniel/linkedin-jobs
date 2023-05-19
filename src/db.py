d1a62c19549b6bd27efdfe4888d0844ac9a9d7dd
Joel Daniel Rico <joeldanielrico@csu.fullerton.edu>
2023-05-18 17:46:08 -0700

    refactor

--- src/db.py
+++ src/db.py
@@ -6,18 +6,15 @@ from job import Job
 def does_exist(job: Job) -> bool:
     """Returns True if job exists in database, False otherwise"""
 
-    conn: sqlite3.Connection = sqlite3.connect("jobs.db")
-    c: sqlite3.Cursor = conn.cursor()
+    with sqlite3.connect("jobs.db") as conn:
+        c: sqlite3.Cursor = conn.cursor()
 
-    c.execute(
-        "SELECT * FROM jobs WHERE title = ? AND company = ? AND location = ?",
-        (job.title, job.company, job.location),
-    )
-    result = c.fetchone()
+        c.execute(
+            "SELECT * FROM jobs WHERE title = ? AND company = ? AND location = ?",
+            (job.title, job.company, job.location),
+        )
 
-    conn.close()
-
-    return result is not None
+        return c.fetchone() is not None
 
 
 def edit_job(
@@ -27,9 +24,6 @@ def edit_job(
 ) -> None:
     """Edits job with specified title"""
 
-    conn: sqlite3.Connection = sqlite3.connect("jobs.db")
-    c: sqlite3.Cursor = conn.cursor()
-
     # Check if job exists in database
     if not does_exist(job):
         raise ValueError("Job does not exist")
@@ -38,7 +32,10 @@ def edit_job(
     if field not in ("title", "company", "link", "location", "date"):
         raise ValueError("Invalid field")
 
-    c.execute(f"UPDATE jobs SET {field} = ? WHERE title = ?", (value, job.title))
+    with sqlite3.connect("jobs.db") as conn:
+        conn.cursor().execute(
+            f"UPDATE jobs SET {field} = ? WHERE title = ?", (value, job.title)
+        )
 
     # Modify job object
     match field:
@@ -53,112 +50,89 @@ def edit_job(
         case "date":
             job.date = value
 
-    conn.commit()
-    conn.close()
-
 
 def delete_job(job: Job) -> None:
     """Deletes job with specified title"""
 
-    conn: sqlite3.Connection = sqlite3.connect("jobs.db")
-    c: sqlite3.Cursor = conn.cursor()
+    if does_exist(job):
+        with sqlite3.connect("jobs.db") as conn:
+            c: sqlite3.Cursor = conn.cursor()
 
-    # Check if job exists in database
-    if not does_exist(job):
-        raise ValueError("Job does not exist")
-
-    try:
-        c.execute("DELETE FROM jobs WHERE title = ?", (job.title,))
-    except sqlite3.Error as e:
-        raise e
-
-    conn.commit()
-    conn.close()
+            try:
+                c.execute("DELETE FROM jobs WHERE title = ?", (job.title,))
+            except sqlite3.Error as e:
+                raise e
 
 
 def get_job(title: str) -> Job:
     """Returns job with specified title"""
 
-    conn = sqlite3.connect("jobs.db")
-    c = conn.cursor()
+    with sqlite3.connect("jobs.db") as conn:
+        c = conn.cursor()
 
-    c.execute("SELECT * FROM jobs WHERE title = ?", (title))
-    result = c.fetchone()
+        c.execute("SELECT * FROM jobs WHERE title = ?", (title))
+        result = c.fetchone()
 
-    conn.close()
+        if result is None:
+            raise ValueError("No job found with that title")
 
-    if result is None:
-        raise ValueError("No job found with that title")
-
-    return Job(result[1], result[2], result[3], result[4], result[5], result[6])
+        return Job(result[1], result[2], result[3], result[4], result[5], result[6])
 
 
 def get_jobs() -> list[Job]:
     """Returns all jobs in database"""
 
-    conn: sqlite3.Connection = sqlite3.connect("jobs.db")
-    c: sqlite3.Cursor = conn.cursor()
+    with sqlite3.connect("jobs.db") as conn:
+        c: sqlite3.Cursor = conn.cursor()
 
-    c.execute("SELECT * FROM jobs")
-    jobs: list[tuple[int, str, int, str, str, int]] = c.fetchall()
+        c.execute("SELECT * FROM jobs")
+        jobs: list[tuple[int, str, int, str, str, int]] = c.fetchall()
 
-    conn.close()
-
-    return [Job(job[1], job[2], job[3], job[4], job[5], job[6]) for job in jobs]
+        return [Job(job[1], job[2], job[3], job[4], job[5], job[6]) for job in jobs]
 
 
 def insert_job(job: Job) -> None:
     """Inserts job into database"""
 
-    conn: sqlite3.Connection = sqlite3.connect("jobs.db")
-    c: sqlite3.Cursor = conn.cursor()
-
     # Check if job already exists in database
     if does_exist(job):
         return
 
-    try:
-        c.execute(
-            "INSERT INTO jobs VALUES (?, ?, ?, ?, ?, ?, ?)",
-            (
-                (str(uuid.uuid4())),
-                job.title,
-                job.company,
-                job.location,
-                job.link,
-                job.date,
-                job.date_added,
-            ),
-        )
-
-        conn.commit()
-        conn.close()
-    except Exception as e:
-        print(f"Error: {e}")
+    with sqlite3.connect("jobs.db") as conn:
+        try:
+            conn.cursor().execute(
+                "INSERT INTO jobs VALUES (?, ?, ?, ?, ?, ?, ?)",
+                (
+                    (str(uuid.uuid4())),
+                    job.title,
+                    job.company,
+                    job.location,
+                    job.link,
+                    job.date,
+                    job.date_added,
+                ),
+            )
+        except Exception as e:
+            print(f"Error: {e}")
 
 
 def initialize_database() -> None:
     """Creates/initializes database"""
 
-    conn: sqlite3.Connection = sqlite3.connect("jobs.db")
-    c: sqlite3.Cursor = conn.cursor()
-
-    # Create table if it does not exist
-    try:
-        c.execute(
-            """CREATE TABLE jobs (
-                    unique_id text primary key,
-                    title text not null,
-                    company text not null,
-                    location text not null,
-                    link text,
-                    date text,
-                    date_added text default current_timestamp
-                )"""
-        )
-    except sqlite3.OperationalError:
-        # Table already exists
-        pass
-
-    conn.commit()
-    conn.close()
+    with sqlite3.connect("jobs.db") as conn:
+        # Create table if it does not exist
+        try:
+            conn.cursor().execute(
+                """CREATE TABLE jobs (
+                        unique_id text primary key,
+                        title text not null,
+                        company text not null,
+                        location text not null,
+                        link text,
+                        date text,
+                        date_added text default current_timestamp
+                    )"""
+            )
+        except sqlite3.OperationalError:
+            # Table already exists
+            pass
